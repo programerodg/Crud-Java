96a10c8d38c2b9053d17b49c7e9d7683d8599ec4
Programerodg <Programerodg@gmail.com>
2025-09-28 23:16:40 -0500

    Commit Inicial

---
+++ src/main/java/com/programerodg/curdjava/CurdJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.programerodg.curdjava;
+import com.programerodg.vista.VistaPrincipal;
+
+/**
+ *
+ * @author omard
+ */
+public class CurdJava {
+
+    public static void main(String[] args) {
+        VistaPrincipal vista =new VistaPrincipal();
+        vista.setVisible(true);
+    }
+}
